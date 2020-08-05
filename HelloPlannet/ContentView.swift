73eb6537c828360eb344b84b37d228f2eb84d10b
Thotsana <thosana.mabotsa@umuzi.org>
2020-08-05 12:44:23 +0200

    Initial Commit

---
+++ HelloPlannet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloPlannet
+//
+//  Created by Thotsana Mabotsa on 2020/08/05.
+//  Copyright © 2020 Thotsana Mabotsa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
